93ff07ee2860b9b212c812dae12dd4dcc2b912ed
Betrand13 <40883736+banks03@users.noreply.github.com>
2022-06-01 13:58:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,51 +1,56 @@
+
 provider "aws" {
-   access_key = ""
-   secret_key = ""
-   region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+  region     = "us-east-1"
 }
 
 variable "ec2-instance" {
-  default = ""
+  type    = string
+  default = "i-0ab6b889c2a736d7d"
+
 }
+
 
 resource "aws_cloudwatch_dashboard" "starter-dashboard" {
   dashboard_name = "dashboard-${var.ec2-instance}"
-  
+
+
   dashboard_body = <<EOF
- {
-   "widgets": {
-       }
-          "type":"metric",
-          "x":0,
-          "y":0,
-          "width":12,
-          "height":6,
-          "properties":{
-             "metrics":[
-                [
-                   "AWS/EC2",
-                   "CPUUtilization",
-                   "InstanceId",
-                   "${var.ec2-instance}"
-                ]
-             ],
-             "period":300,
-             "stat":"Average",
-             "region":"us-east-1",
-             "title":"${var.ec2-instance} - CPU Utilization"
-          }
-       },
-       {
-          "type":"text",
-          "x":0,
-          "y":7,
-          "width":3,
-          "height":3,
-          "properties":{
-             "markdown":"some text"
-          }
-      },
-      {
+{
+  "widgets": [
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/EC2",
+            "CPUUtilization",
+            "InstanceId",
+            "${var.ec2-instance}"
+          ]
+        ],
+        "period": 300,
+        "stat": "Average",
+        "region": "us-east-1",
+        "title": "${var.ec2-instance} - CPU Utilization"
+      }
+    },
+    {
+      "type": "text",
+      "x": 0,
+      "y": 7,
+      "width": 3,
+      "height": 3,
+      "properties": {
+        "markdown": "Hello world"
+      }
+    },
+     {
          "type":"metric",
          "x":0,
          "y":0,
@@ -66,10 +71,12 @@ resource "aws_cloudwatch_dashboard" "starter-dashboard" {
             "title":"${var.ec2-instance} - NetworkIn"
          }
       }
-   ]
- }         
- EOF
+
+  ]
 }
+EOF
+}
+
 resource "aws_cloudwatch_metric_alarm" "ec2-cpu-88" {
   alarm_name                = "terraform-test-ec2-cpu-80"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
@@ -80,5 +87,5 @@ resource "aws_cloudwatch_metric_alarm" "ec2-cpu-88" {
   statistic                 = "Average"
   threshold                 = "80"
   alarm_description         = "This metric monitors when ec2 cpu utilization reaches 80"
-  insufficient_data_actions  = []
+  insufficient_data_actions = []
 }
